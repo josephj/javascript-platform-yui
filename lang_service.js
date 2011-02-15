102342750b20567e6eefcb497eb8021e8c93a065
Joseph Chiang <josephj6802@gmail.com>
2011-02-15 00:53:15 -0500

    Add documentation and logging

--- lang_service.js
+++ lang_service.js
@@ -1,54 +1,120 @@
 YUI.add("lang-service", function (Y) {
-    var moduleName, // language module (demo)
-        activeTag,  // current language tag (zh-TW, zh-CN)
-        trans;      // translation object (trans["key"]);
+
+    var _moduleName, // Language module (demo)
+        _activeTag;  // Current language tag (zh-TW, zh-CN)
+
     /*
-     * Get current using language module
+     * Get current language module name.
+     * 
+     * @method getLangModule
+     * @static
+     * @public
+     * @return {String} Language module name.
      */
     Y.PlatformCore.getLangModule = function () {
-        return moduleName;
+        Y.log("getLangModule() is executed. Current langauge module name is " + _moduleName, "info", "Y.PlatformCore");
+        return _moduleName;
     };
+
     /*
-     * Set which module you want to use.
+     * Set language module.
+     * It switches the _activeTag variable.
+     *
+     * @method setLangModule
+     * @static
+     * @public
+     * @return void();
      */
     Y.PlatformCore.setLangModule = function (name) {
-        moduleName = name;
-        activeTag = Y.Intl.getLang(name);
+        Y.log("setLangModule() is executed. Input value = " + name, "info", "Y.PlatformCore");
+        _moduleName = name;
+        _activeTag = Y.Intl.getLang(name);
+        if (typeof _activeTag === "undefined") {
+            Y.log("setLangModule('" + name + "') - It fails to set language module.", "warn", "Y.PlatformCore");
+            return false;
+        } else {
+            Y.log("setLangModule('" + name + "') - It executes successfully.", "info", "Y.PlatformCore");
+            return true;
+        }
     };
+
     /*
-     * Sandbox can set active language tag.
+     * Switch to different language tag. 
+     * It will on-demand load target language resource file.
+     * 
+     * @method setLang
+     * @static
+     * @public
+     * @param {String} lang Language tag.
+     * @param {Function} callback Callback function.
+     * @return void();
      */
     Y.PlatformSandbox.prototype.setLang = function (lang, callback) {
-        activeTag = lang;
-        Y.use("lang/" + moduleName + "_" + lang, function(Y) {
-            if (Y.Intl.setLang(moduleName, lang) && callback) {
+        Y.log("setLang() is executed.", "info", "Y.PlatformSandbox");
+        _activeTag = lang;
+        Y.use("lang/" + _moduleName + "_" + lang, function(Y) {
+            if (Y.Intl.setLang(_moduleName, lang) && callback) {
                 callback();
             }
         });
     };
+
     /*
-     * Sandbox can get active language tag.
+     * Get current language tag.
+     * 
+     * @method getLang
+     * @static
+     * @public
+     * @return {String} Current language tag.
      */
-    Y.PlatformSandbox.prototype.getLang = function (tag) {
-        return activeTag;
+    Y.PlatformSandbox.prototype.getLang = function () {
+        Y.log("getLang() is executed.", "info", "Y.PlatformSandbox");
+        return _activeTag;
     };
+
     /*
-     * Use language key to get translation
+     * Get translation by key.
+     * 
+     * @method getTrans
+     * @static
+     * @public
+     * @return {String} Current language tag.
      */
     Y.PlatformSandbox.prototype.getTrans = function (key, defaultValue, tokens) {
+        Y.log("getLang() is executed.", "info", "Y.PlatformSandbox");
         var isExist,
             trans,
             tokens,
             text;
-        key = moduleName + "-" + this.id.replace(/\-/g, "_") + "-" + key;
-        defaultValue = defaultValue || "";
-        trans = Y.Intl.get(moduleName);
-        isExist =  (typeof trans[key] !== "undefined") ? true : false;
+
         tokens = tokens || null;
-        text = (isExist) ? trans[key] : defaultValue;
+        defaultValue = defaultValue || "";
+
+        // Make sure module name has been defined.
+        if (typeof _moduleName === "undefined") {
+            Y.log("getLang() fails because module is not defined.", "error", "Y.PlatformSandbox");
+            return;
+        }
+
+        // Full language key is composed by module name, div id, and key.
+        key = _moduleName + "-" + this.id.replace(/\-/g, "_") + "-" + key;
+
+        // Get tranlation resource array.
+        trans = Y.Intl.get(_moduleName);
+
+        // Check if the key exists in language resource.
+        isExist =  (typeof trans[key] !== "undefined") ? true : false;
+        if (isExist) {
+            text = trans[key];
+        } else {
+            Y.log("getLang() - This language key '" + key + "' has not been translated yet.", "warn", "Y.PlatformSandbox");
+            text = defaultValue;
+        }
+
         if (tokens) {
             text = Y.substitute(text, tokens);
         }
         return text;
     };
-}, "3.1.1", {use: ["intl", "substitute"]});
+
+});
